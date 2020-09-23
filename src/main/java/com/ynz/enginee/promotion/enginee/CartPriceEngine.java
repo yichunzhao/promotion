b02e369b41efbd4f74ea842a4fde2d031ac27327
yichunzhao <zhaoyichun@hotmail.com>
2020-09-23 17:04:04 +0200

    class renaming

--- src/main/java/com/ynz/enginee/promotion/enginee/CartPriceEngine.java
+++ src/main/java/com/ynz/enginee/promotion/enginee/CartPriceEngine.java
@@ -1,12 +1,15 @@
 package com.ynz.enginee.promotion.enginee;
 
 import com.ynz.enginee.promotion.domain.Cart;
+import lombok.Getter;
 
 import java.util.Map;
 
 
 public class CartPriceEngine implements Payable {
     private final Map<Character, Integer> priceMap;
+
+    @Getter
     private final Cart cart;
 
     public CartPriceEngine(Cart cart, Map<Character, Integer> priceMap) {
--- src/main/java/com/ynz/enginee/promotion/enginee/promotions/BuySKUCombinations.java
+++ src/main/java/com/ynz/enginee/promotion/enginee/promotions/BuySKUCombinations.java
@@ -3,8 +3,8 @@ package com.ynz.enginee.promotion.enginee.promotions;
 import com.ynz.enginee.promotion.enginee.AbstractPromotion;
 import com.ynz.enginee.promotion.enginee.CartPriceEngine;
 
-public class BuySKUOneAndTWO extends AbstractPromotion {
-    public BuySKUOneAndTWO(CartPriceEngine engine) {
+public class BuySKUCombinations extends AbstractPromotion {
+    public BuySKUCombinations(CartPriceEngine engine) {
         super(engine);
     }
 
